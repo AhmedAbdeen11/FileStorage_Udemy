27996140783a83f1c3176eab9c064b47ff1f6235
A7MED-Freelancer <aabdeen@fcih1.com>
2017-07-08 04:24:44 +0200

    Second Commit

--- app/src/main/java/com/freelancer/ahmedabdeen11/filestorage_udemy/SecondActivity.java
+++ app/src/main/java/com/freelancer/ahmedabdeen11/filestorage_udemy/SecondActivity.java
@@ -3,13 +3,11 @@ package com.freelancer.ahmedabdeen11.filestorage_udemy;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_second);
     }
 }
