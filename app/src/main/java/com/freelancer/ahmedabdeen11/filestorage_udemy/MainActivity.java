75b38cab92c986ff415b2a3f8a4d0b55eb476db8
A7MED-Freelancer <aabdeen@fcih1.com>
2017-07-08 04:52:46 +0200

    Final Commit

--- app/src/main/java/com/freelancer/ahmedabdeen11/filestorage_udemy/MainActivity.java
+++ app/src/main/java/com/freelancer/ahmedabdeen11/filestorage_udemy/MainActivity.java
@@ -1,15 +1,38 @@
 package com.freelancer.ahmedabdeen11.filestorage_udemy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        userName = (EditText) findViewById(R.id.et_username);
+    }
 
+    public void saveUsername(View view) throws IOException{
+
+        String usernameValue = userName.getText().toString();
+        FileOutputStream fileOutputStream = openFileOutput("file.txt", MODE_PRIVATE);
+        fileOutputStream.write(usernameValue.getBytes());
+        fileOutputStream.close();
+        Toast.makeText(this, "Saved file " + getFilesDir().toString() + "/file.txt", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void goToNext(View view) {
+        startActivity(new Intent(getApplicationContext(), SecondActivity.class));
     }
 }
--- app/src/main/java/com/freelancer/ahmedabdeen11/filestorage_udemy/SecondActivity.java
+++ app/src/main/java/com/freelancer/ahmedabdeen11/filestorage_udemy/SecondActivity.java
@@ -2,12 +2,37 @@ package com.freelancer.ahmedabdeen11.filestorage_udemy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class SecondActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        tv = (TextView) findViewById(R.id.tv);
+    }
+
+    public void loadUserName(View view) throws IOException{
+
+        FileInputStream fileInputStream = openFileInput("file.txt");
+        int read = -1;
+        StringBuffer buffer = new StringBuffer();
+        while ((read = fileInputStream.read()) != -1){
+
+            buffer.append((char) read);
+
+        }
+
+        tv.setText(buffer.toString());
+        fileInputStream.close();
+
     }
 }
